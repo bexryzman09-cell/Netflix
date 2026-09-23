c7be41279570ca563d4ec80ef1b69936be0702da
bexryzman09-ceel <bexryzman09@gmail.com>
2026-09-23 14:49:41 +0500

    create

--- src/pages/ProfileGatePage.tsx
+++ src/pages/ProfileGatePage.tsx
@@ -109,7 +109,7 @@ export default function ProfileGatePage() {
 
                 {mode === "register" && (
                     <div className="max-w-sm mx-auto">
-                        <h1 className="text-white text-2xl sm:text-3xl font-semibold text-center mb-2">
+                        <h1 className= "flex text-white text-2xl sm:text-3xl font-semibold text-center mb-2">
                            Создать профиль
                         </h1>
                         <p className="text-white/50 text-center mb-8 text-sm sm:text-base">
